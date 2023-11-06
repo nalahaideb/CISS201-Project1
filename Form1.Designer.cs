bdb6ea352f7c35bb3a3f9cba28af4a7f96d1eb41
nasser <nasser@DESKTOP-ET92O4P>
2023-11-06 10:32:09 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            inventoryButton = new Button();
+            SuspendLayout();
+            // 
+            // inventoryButton
+            // 
+            inventoryButton.Location = new Point(463, 278);
+            inventoryButton.Name = "inventoryButton";
+            inventoryButton.Size = new Size(75, 23);
+            inventoryButton.TabIndex = 0;
+            inventoryButton.Text = "Inventory";
+            inventoryButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(646, 450);
+            Controls.Add(inventoryButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button inventoryButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
